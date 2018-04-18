31196b1dee62c411e859a1de06348b5aa7bd6436
Andreo Vieira <andreo.vieira@ilegra.com>
2018-04-18 17:43:07 -0300

    Remove unused code

--- app/containers/App/App.js
+++ app/containers/App/App.js
@@ -8,7 +8,7 @@
 
 import React from 'react';
 import { Helmet } from 'react-helmet';
-import { Switch, Route } from 'react-router-dom';
+// import { Switch, Route } from 'react-router-dom';
 
 import Header from 'components/Header';
 import Footer from 'components/Footer';
