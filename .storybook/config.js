7d1c4ac00f6e0c45d26bb59c7238f005dd7c5c8f
Hyan Mandian <hyanmandian@hotmail.com>
2018-07-31 17:44:13 -0300

    Update storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { configure, addDecorator } from '@storybook/react';
 import { ThemeProvider } from 'styled-components';
 
+import './styles.css';
+
 import { theme } from '../src/globalStyles';
 
 const req = require.context('../src/components', true, /stories.js$/);
@@ -11,9 +13,9 @@ function loadStories() {
 }
 
 addDecorator(story => (
-  <ThemeProvider theme={theme}>
-    {story()}
-  </ThemeProvider>
+  <div className="storybook-content">
+    <ThemeProvider theme={theme}>{story()}</ThemeProvider>
+  </div>
 ));
 
 configure(loadStories, module);
