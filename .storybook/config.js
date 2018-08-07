87696e6d3b05af1560891520651d7668c66c263c
Hyan Mandian <hyanmandian@hotmail.com>
2018-08-07 18:13:43 -0300

    Update storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { configure, addDecorator } from '@storybook/react';
+import { configure, addDecorator, setAddon } from '@storybook/react';
+import withBackgrounds from '@storybook/addon-backgrounds';
 import { ThemeProvider } from 'styled-components';
 
 import './styles.css';
@@ -11,6 +12,19 @@ const req = require.context('../src/components', true, /stories.js$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
+
+addDecorator(
+  withBackgrounds([
+    {
+      name: 'Pattern',
+      value:
+        'repeating-linear-gradient(0deg,transparent,transparent 7px,rgba(0,0,0,.2) 1px,transparent 8px),repeating-linear-gradient(90deg,transparent,transparent 7px,rgba(0,0,0,.2) 1px,transparent 8px)',
+      default: true,
+    },
+    { name: 'Bright', value: '#FFF' },
+    { name: 'Dark', value: '#000' },
+  ])
+);
 
 addDecorator(story => (
   <div className="storybook-content">
