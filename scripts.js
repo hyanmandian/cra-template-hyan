df924f48d2886ab7445122334e7a6894edfe1262
Hyan Mandian <hyanmandian@hotmail.com>
2018-08-09 19:39:49 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -9,8 +9,8 @@ process.stdin.resume();
 process.stdin.setEncoding('utf8');
 process.stdout.write('\n');
 
-function onEnd() {
-  process.stdout.write('\n\nDone!\n\n');
+function onEnd(msg) {
+  process.stdout.write(`\n\n${msg}\n\n`);
   process.exit(0);
 }
 
@@ -18,14 +18,17 @@ function onInstallDependencies() {
   process.stdout.write('\n\n');
 
   rimraf('.git/', () => {
-    exec('git init && git add . && git commit -m "Initial commit"', onEnd);
+    exec(
+      'git init && git add . && git commit -m "Initial commit"',
+      onEnd.bind(this, 'Done!')
+    );
   });
 }
 
 if (task === 'setup') {
   process.stdout.write('Installing dependencies... (This might take a while)');
 
-  exec('yarn --version', (err, stdout, stderr) => {
+  return exec('yarn --version', (err, stdout, stderr) => {
     if (parseFloat(stdout) < 0.15 || err || process.env.USE_YARN === 'false') {
       exec('npm install', onInstallDependencies);
     } else {
@@ -36,3 +39,5 @@ if (task === 'setup') {
 
 if (task === 'clean') {
 }
+
+onEnd('Invalid task!');
