0ee87f3c82458d661ccc9a539d678ec2617bf7b3
Hyan Mandian <hyanmandian@hotmail.com>
2018-08-09 20:01:07 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -75,7 +75,7 @@ if (task === 'clean') {
     },
   ];
 
-  filesToRemove.forEach(rimraf.sync);
+  filesToRemove.forEach(file => rimraf.sync(file));
 
   filesToReplace.forEach(({ file, replace }) => {
     fs.readFile(file, 'utf8', (err, data) => {
