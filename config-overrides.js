13c82e568aed06c5215306d658a7e14df607dc19
Hyan Mandian <hyanmandian@hotmail.com>
2018-12-18 12:07:41 -0200

    feat(structure): add babel-emotion-plugin

--- config-overrides.js
+++ config-overrides.js
@@ -1,10 +1,13 @@
 const path = require('path');
+const { injectBabelPlugin } = require('react-app-rewired');
 
 module.exports = (config, env) => {
   config.resolve.alias = {
     ...config.resolve.alias,
     '@': path.resolve('./src'),
   };
+
+  injectBabelPlugin('emotion', config);
 
   return config;
 };
